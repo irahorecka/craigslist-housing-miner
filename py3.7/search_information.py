76da211abe9c0f30c67d6e2c89a92d710c6d426b
Ira Horecka <ira89@icloud.com>
2019-09-11 22:17:03 -0700

    create py3.5 and py3.7 folder for python ver 3.5 and 3.7 - reconfig 3.5 for lack of f-string

---
+++ py3.7/search_information.py
@@ -0,0 +1,8 @@
+#a .py file to outline selection parameters when scraping CL api.
+
+class SelectionKeys:
+    state_keys = ['california']
+    #selected_reg = ['sfbay']
+    #selected_cat = ['sub', 'roo']
+    #district_list = ['oakland','berkeley','richmond','el cerrito','san leandro','alameda','albany','hercules']
+    #name = 'ira'
