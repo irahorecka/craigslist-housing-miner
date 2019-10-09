7322f7dce38b091aa4ccf0d3a9762d145700941c
Ira Horecka <ira89@icloud.com>
2019-10-09 12:15:23 -0700

    allow more dynamic user input information

--- py3.7/FLASK/application.py
+++ py3.7/FLASK/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from models import *
 from webscrape import *
 from sqlalchemy import func
@@ -17,17 +17,20 @@ def index():
 def initial():
     state = request.form.get('state_id')
     district = request.form.get("district_id")
+    print(state, district)
     craigslist = Post.query.filter(func.lower(Post.cl_state)==func.lower(state)).filter(func.lower(Post.location).like("%"+func.lower(district)+"%")).order_by(Post.date_posted.desc()).limit(50)
-    return render_template("initial.html", craigslist = craigslist)
-
-@app.route("/post", methods=["POST"])
-def post():
     """Jot a name."""
 
     # Get form information.
     name = request.form.get("name")
+    return render_template("initial.html", craigslist = craigslist, district = district, state = state, name = name)
+
+@app.route("/post", methods=["POST"])
+def post():
+    var = (request.form.get("post_id").split(' '))
+    print(var, type(var))
     try:
-        post_id = int(request.form.get("post_id"))
+        post_id = int(var[0])
     except ValueError:
         return render_template("error.html", message="Invalid post number.")
 
@@ -37,16 +40,28 @@ def post():
         return render_template("error.html", message="No such post with that id.")
 
     # Add passenger.
-    post.add_candidate(name)
     passengers = Users.query.filter_by(post_id=post_id).all()
-    return render_template("flight.html", post = post, passengers = passengers, test=return_body(post.url))
+    return render_template("flight.html", post = post, name = var[1], passengers = passengers, test=return_body(post.url), state = var[2], district = var[3])
 
 
-'''@app.route("/posts")
-def posts(district):
-    """List all posts."""
-    posts = Post.query.filter(Post.cl_district==district.title()).order_by(Post.date_posted.desc()).limit(50)
-    return render_template("flights.html", posts=posts)'''
+@app.route("/posts", methods=["POST"])
+def posts():
+    var = (request.form.get("name").split(' '))
+    print(var)
+    try:
+        post_id = int(var[0])
+    except ValueError:
+        return render_template("error.html", message="Invalid post number.")
+
+    # Make sure the flight exists.
+    post = Post.query.get(post_id)
+    if not post:
+        return render_template("error.html", message="No such post with that id.")
+
+    # Add passenger.
+    post.add_candidate(var[1].title()) #name
+    return render_template('success.html', name = var[1], state = var[2], district = var[3])
+    #return render_template("flight.html", post = post, name = var[1], passengers = passengers, test=return_body(post.url))
 
 '''@app.route("/posts/<int:post_id>")
 def post(post_id):
