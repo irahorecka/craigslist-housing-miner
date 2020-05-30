5395a190c21de5fe14034bf97cb9b530955a894f
Ira Horecka <ira89@icloud.com>
2020-05-30 09:31:28 -0700

    refactor code for cleanliness - blackify code

--- multiprocessing_cl.py
+++ multiprocessing_cl.py
@@ -6,7 +6,6 @@ import csv
 import copy
 import datetime
 import time
-import logging
 import os
 import pandas as pd
 import random
@@ -15,7 +14,8 @@ import queue
 from craigslist import CraigslistHousing
 from cl_information import Filters as clsd
 from search_information import SelectionKeys as sk
-from compiled_set import Search
+from cl_regions import Regions
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = f"{BASE_DIR}/Data/{datetime.date.today()}"
 
@@ -26,8 +26,7 @@ class CLHousingSelect:
     encompasses methods to set search instance, execute
     search, and write search information to .csv files. """
 
-    def __init__(self, country, inst_category,
-                 inst_filters, code_break, _geotag):
+    def __init__(self, country, inst_category, inst_filters, code_break, _geotag):
         self.country = country
         self.inst_category = inst_category
         self.inst_filters = inst_filters
@@ -38,10 +37,8 @@ class CLHousingSelect:
     def small_region(self, inst_site):
         try:
             self.cl_instance = CraigslistHousing(
-                site=inst_site,
-                category=self.inst_category,
-                filters=self.inst_filters
-                )
+                site=inst_site, category=self.inst_category, filters=self.inst_filters
+            )
         except Exception as e:  # commonly requests.ConnectionError, requests.ConnectionResetError
             handle_error(e)
 
@@ -52,28 +49,27 @@ class CLHousingSelect:
                 area=inst_area,
                 category=self.inst_category,
                 filters=self.inst_filters,
-                )
+            )
         except Exception as e:
             handle_error(e)
 
-    def parse_search(self, header_list,
-                     state, region, sub_region, category):
+    def parse_search(self, header_list, state, region, sub_region, category):
         try:
             header_list.extend(
-                [f"{self.country}{self.code_break}{state}{self.code_break}{region}{self.code_break}"
-                 f"{sub_region}{self.code_break}{category}{self.code_break}"
-                 f"{i['id']}{self.code_break}{i['repost_of']}{self.code_break}"
-                 f"{i['name']}{self.code_break}{i['url']}{self.code_break}"
-                 f"{i['datetime'][0:10]}{self.code_break}{i['datetime'][11:]}{self.code_break}"
-                 f"{i['price']}{self.code_break}{i['where']}{self.code_break}"
-                 f"{i['has_image']}{self.code_break}{i['geotag']}{self.code_break}"
-                 f"{i['bedrooms']}{self.code_break}{i['area']}"
-                 for i in self.cl_instance.get_results(
-                     sort_by=None,
-                     geotagged=self.geotag,
-                     limit=None
-                 )]
-                )
+                [
+                    f"{self.country}{self.code_break}{state}{self.code_break}{region}{self.code_break}"
+                    f"{sub_region}{self.code_break}{category}{self.code_break}"
+                    f"{i['id']}{self.code_break}{i['repost_of']}{self.code_break}"
+                    f"{i['name']}{self.code_break}{i['url']}{self.code_break}"
+                    f"{i['datetime'][0:10]}{self.code_break}{i['datetime'][11:]}{self.code_break}"
+                    f"{i['price']}{self.code_break}{i['where']}{self.code_break}"
+                    f"{i['has_image']}{self.code_break}{i['geotag']}{self.code_break}"
+                    f"{i['bedrooms']}{self.code_break}{i['area']}"
+                    for i in self.cl_instance.get_results(
+                        sort_by=None, geotagged=self.geotag, limit=None
+                    )
+                ]
+            )
             if len(header_list) == 1:
                 print(state, region, "NO POSTS**")
                 # return None
@@ -83,43 +79,28 @@ class CLHousingSelect:
         except (AttributeError, OSError) as e:
             handle_error(e)
 
-    def write_to_file(self, write_list,
-                      inst_site_name, inst_state_name):
+    def write_to_file(self, write_list, inst_site_name, inst_state_name):
         if not os.path.exists(DATA_DIR):
             os.makedirs(DATA_DIR)
         else:
             pass
         os.chdir(DATA_DIR)
         title = f"{datetime.date.today()}_geotag_{self.geotag}_craigslist_{self.inst_category}_{inst_site_name}_{inst_state_name}.csv"
-        with open(title, 'w', newline='') as rm_csv:
-            writer = csv.writer(rm_csv, delimiter=',')
+        with open(title, "w", newline="") as rm_csv:
+            writer = csv.writer(rm_csv, delimiter=",")
             try:
-                writer.writerows([
-                    i.split(self.code_break) for i in write_list
-                    ])
+                writer.writerows([i.split(self.code_break) for i in write_list])
             except TypeError as e:
                 handle_error(e)
         os.chdir(BASE_DIR)
 
 
-def my_logger(func):
-    logging.basicConfig(filename=f"{func.__name__}.log", level=logging.INFO)
-
-    def wrapper(*args, **kwargs):
-        date_time = str(datetime.datetime.now())[:-10]
-        log_dict = clsd.extra_filters
-        logging.info(
-            f"Ran with filters: {log_dict} at {date_time}")
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
-class ExecSearch:
+class ExecuteSearch:
     """ A class to prepare search parameters by parsing
     search information into a format compatible with
     python-craigslist module. This class operates closely
     with CLHousingSelect. """
+
     def __init__(self, country, house_filter, q, d):
         self.q = q
         self.d = d
@@ -136,11 +117,10 @@ class ExecSearch:
             f"Price{self.code_break}Location{self.code_break}"
             f"Post has Image{self.code_break}Post has Geotag{self.code_break}"
             f"Bedrooms{self.code_break}Area"
-            ]
-    
-    @my_logger
+        ]
+
     def cl_search(self, tup):
-        housing_dict = clsd.cat_dict
+        housing_dict = clsd.housing_categories
         if len(tup) == 3:
             self.area(tup, housing_dict)
         else:
@@ -152,9 +132,7 @@ class ExecSearch:
             if cat not in self.house_filter:
                 continue
             else:
-                result = self.search_package(
-                    state, reg, cat, _geotag=self.d['geotag']
-                    )
+                result = self.search_package(state, reg, cat, _geotag=self.d["geotag"])
             if not result:
                 continue
         self.concat_csv(state, reg)
@@ -166,18 +144,17 @@ class ExecSearch:
                 continue
             else:
                 result = self.search_package(
-                    state, reg, cat, area=sub_reg, _geotag=self.d['geotag']
-                    )
+                    state, reg, cat, area=sub_reg, _geotag=self.d["geotag"]
+                )
             if not result:
                 continue
         self.concat_csv(state, sub_reg)
 
-    def search_package(self, state,
-                       reg, cat, _geotag, area=""):
+    def search_package(self, state, reg, cat, _geotag, area=""):
         header_list = copy.deepcopy(self.header)
         housing_result = CLHousingSelect(
             self.country, cat, self.room_filter, self.code_break, _geotag
-            )
+        )
         if area == "":
             area = reg
             housing_result.small_region(reg)
@@ -189,11 +166,11 @@ class ExecSearch:
             else:
                 append_list = housing_result.parse_search(
                     header_list, state, reg, area, cat
-                    )
+                )
                 if not append_list:
                     return None
                 housing_result.write_to_file(append_list, area, state)
-                return 'OK'
+                return "OK"
         except AttributeError:
             pass
 
@@ -217,24 +194,20 @@ class ExecSearch:
                         cat_file = cat_file.append(pd.read_csv(i[1]))
                     os.remove(i[1])
                 else:
-                    print('no_file')
+                    print("no_file")
             try:
-                cat_file.to_csv(f"CraigslistHousing_{state}_{region}.csv",index=False)
+                cat_file.to_csv(f"CraigslistHousing_{state}_{region}.csv", index=False)
             except UnboundLocalError as error:
                 handle_error(error)
 
 
 def handle_error(error):
     print(f"There was an error:: {error} - nothing written to file.")
-    logging.basicConfig(
-        filename=f'{BASE_DIR}/CL_Mining_Error.log', level=logging.INFO
-        )
-    logging.info(f"Time:: {datetime.datetime.today()}\nError:: {error}")
     time.sleep(5)  # provide 5 sec wait time before continuing search
 
 
 def execute_search(country, state, q, d):
-    search_criteria = ExecSearch(country, sk.selected_cat, q, d)
+    search_criteria = ExecuteSearch(country, sk.selected_cat, q, d)
     search_criteria.cl_search(state)
 
 
@@ -242,26 +215,26 @@ def main(_geotag):
     manager = mp.Manager()
     q = manager.Queue()
     d = manager.dict()
-    d['geotag'] = _geotag
+    d["geotag"] = _geotag
     pool = mp.Pool(mp.cpu_count() + 2)
     jobs = []
-    country_list = [country for country in Search.__dict__ if country[0:2] != '__']
+    country_list = [country for country in Regions.__dict__ if country[0:2] != "__"]
     for country in country_list:
-        for province in eval(f'Search.{country}'):
+        for province in eval(f"Regions.{country}"):
             job = pool.apply_async(execute_search, (country, province, q, d))
             jobs.append(job)
     try:
         for job in jobs:
-            job.get(3000) # timeout after 3000 sec
+            job.get(3000)  # timeout after 3000 sec
     except mp.TimeoutError:
         print("process exceeded 10 minutes: mutliprocessing terminated.")
         pool.terminate()
     print("process complete")
 
 
-if __name__ == '__main__':
-    GEOTAG_INPUT = (input(
-        "Would you like to search for geotagged results?[y/n]: "
-        )).lower()
-    GEOTAG = True if GEOTAG_INPUT == 'y' else False
+if __name__ == "__main__":
+    GEOTAG_INPUT = (
+        input("Would you like to search for geotagged results?[y/n]: ")
+    ).lower()
+    GEOTAG = True if GEOTAG_INPUT == "y" else False
     main(GEOTAG)
--- search_information.py
+++ search_information.py
@@ -1,4 +1,19 @@
-#a .py file to outline selection parameters when scraping CL api.
+"""py file to search these selected subcategories in Craigslist Housing.
+Omit items from selected_cat if undesired."""
 
 class SelectionKeys:
-    selected_cat = ['apa', 'swp', 'off', 'prk', 'reb', 'reo', 'roo', 'sub', 'vac', 'hou', 'rew', 'sha', 'sbw'] # e.g. ['apa', 'roo'] - search_cl.py will handle boston and newyork exceptions
+    selected_cat = [
+        "apa",
+        "swp",
+        "off",
+        "prk",
+        "reb",
+        "reo",
+        "roo",
+        "sub",
+        "vac",
+        "hou",
+        "rew",
+        "sha",
+        "sbw"
+    ]  # e.g. ['apa', 'roo'] - multiprocessing_cl.py will handle boston and newyork exceptions
